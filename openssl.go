19ce37452af9bc67bbd21a2bc992020aa5091516
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2022-01-26 13:06:54 +0330

    handel error

--- openssl.go
+++ openssl.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"strings"
 )
 
 func SslDecrypt(name, filename string) (out string, errOut error) {
 	args := []string{"smime", "-decrypt", "-in", name, "-inform", "DER", "-inkey", "key/key.private", "-out", "decrypt/" + filename, "-binary"}
 	output, err_ := RunCMD("openssl", args, true)
 	if err_ != nil {
-		return "", fmt.Errorf(output)
+		return "", err_
 	}
 	return output, nil
 }
@@ -21,11 +20,8 @@ func RunCMD(path string, args []string, debug bool) (out string, err error) {
 	var b []byte
 	b, err = cmd.CombinedOutput()
 	out = string(b)
-	if debug {
-		errMsg := strings.Join(cmd.Args[:], " ")
-		if err != nil {
-			return "", fmt.Errorf(errMsg)
-		}
+	if err != nil {
+		return "", fmt.Errorf("The private key is incorrect")
 	}
 	return out, nil
 }
