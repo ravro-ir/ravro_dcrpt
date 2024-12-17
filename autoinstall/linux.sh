cc4e570ef32294d27d0d3f933b52f176a992b0ef
raminfp <ramin.blackhat@gmail.com>
2024-12-17 12:41:06 +0330

    Update autorun install for windows and linux

--- autoinstall/linux.sh
+++ autoinstall/linux.sh
@@ -2,9 +2,9 @@
 # Install packages
 apt update && apt install build-essential checkinstall zlib1g-dev openssl libssl-dev unzip -y
 ## Download wkhtmltox
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-tar vxf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-cp wkhtmltox/bin/wk* /usr/local/bin/
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
+sudo ldconfig
 ## Download ravro_dcrpt
 wget https://github.com/ravro-ir/ravro_dcrpt/releases/download/v1.0.3/linux_x64_ravro_dcrpt.zip
 unzip linux_x64_ravro_dcrpt.zip
--- autoinstall/win64.bat
+++ autoinstall/win64.bat
@@ -1,30 +1,11 @@
 // Download OpenSSL
-echo [+++] Downloading OpenSSL ....
-powershell -command "Invoke-WebRequest -OutFile openssl.exe -Uri https://slproweb.com/download/Win64OpenSSL_Light-3_0_5.exe"
-
-// Check if exit
-if exist "C:\Program Files\OpenSSL-Win64\bin\" (
-  echo OpenSSL is installed 
-) else (
-  openssl.exe
-)
-
-// Set Path variable envirment
-setx PATH ^%PATH^%;"C:\Program Files\OpenSSL-Win64\bin"
-
+echo [+++] Downloading OpenSSL Lib ....
+powershell -command "Invoke-WebRequest -OutFile openssl.exe -Uri https://github.com/ravro-ir/ravro_dcrpt/blob/develop/lib/libcrypto-3-x64.dll"
+powershell -command "Invoke-WebRequest -OutFile openssl.exe -Uri https://github.com/ravro-ir/ravro_dcrpt/blob/develop/lib/libssl-3-x64.dll"
 
 // Download wkhtmltox
-echo [+++] Downloading wkhtmltox ....
-powershell -command "Invoke-WebRequest -OutFile wkhtmltox.exe -Uri https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.msvc2015-win64.exe"
-
-// Check if exist
-if exist "C:\Program Files\wkhtmltopdf\bin\" (
-  echo wkhtmltox is installed 
-) else (
-  wkhtmltox.exe
-)
-// Set path 
-setx PATH ^%PATH^%;"C:\Program Files\wkhtmltopdf\bin"
+echo [+++] Downloading wkhtmltox Lib....
+powershell -command "Invoke-WebRequest -OutFile wkhtmltox.exe -Uri https://github.com/ravro-ir/ravro_dcrpt/blob/develop/lib/wkhtmltox.dll"
 
 // Download ravro_dcrpt
 echo [+++] Ravro Decrypt Tools ....
