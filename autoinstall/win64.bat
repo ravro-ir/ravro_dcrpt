807daceb6bcd1fb8580f6d9cf9227504c861d0f4
raminfp <ramin.blackhat@gmail.com>
2024-12-21 16:43:38 +0330

    Update README

--- autoinstall/win64.bat
+++ autoinstall/win64.bat
@@ -3,14 +3,14 @@
 echo [+++] Downloading OpenSSL Libraries...
 powershell -command "& {
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -OutFile libcrypto-3-x64.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/develop/lib/libcrypto-3-x64.dll
-    Invoke-WebRequest -OutFile libssl-3-x64.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/develop/lib/libssl-3-x64.dll
+    Invoke-WebRequest -OutFile libcrypto-3-x64.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/main/lib/libcrypto-3-x64.dll
+    Invoke-WebRequest -OutFile libssl-3-x64.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/main/lib/libssl-3-x64.dll
 }"
 
 echo [+++] Downloading wkhtmltox Library...
 powershell -command "& {
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -OutFile wkhtmltox.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/develop/lib/wkhtmltox.dll
+    Invoke-WebRequest -OutFile wkhtmltox.dll -Uri https://github.com/ravro-ir/ravro_dcrpt/raw/main/lib/wkhtmltox.dll
 }"
 
 echo [+++] Downloading Ravro Decrypt Tools...
