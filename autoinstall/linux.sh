c6a4168580d122289f8e2ad4c53a668761aeb23f
raminfp <ramin.blackhat@gmail.com>
2024-12-21 16:10:13 +0330

    Update auto install linux

--- autoinstall/linux.sh
+++ autoinstall/linux.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "[+++] Updating package lists and installing dependencies..."
 sudo apt update -y
-sudo apt install -y build-essential checkinstall zlib1g-dev openssl libssl-dev unzip wget
+sudo apt install -y build-essential checkinstall zlib1g-dev openssl libssl-dev unzip wget xfonts-75dpi
 
 # Attempt to install wkhtmltopdf
 echo "Installing wkhtmltopdf..."
@@ -15,8 +15,25 @@ if [ $? -ne 0 ]; then
     echo "Resolving unmet dependencies..."
     sudo apt --fix-broken install -y
     echo "Retrying installation of wkhtmltopdf..."
-    sudo apt-get install -y wkhtmltopdf
-     # Define the URL for the wkhtmltox package
+    # Define the URL for the wkhtmltox package
+    PACKAGE_URL="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb"
+    PACKAGE_NAME="wkhtmltox_0.12.6-1.focal_amd64.deb"
+
+    # Download the package
+    echo "Downloading $PACKAGE_NAME..."
+    wget -O $PACKAGE_NAME $PACKAGE_URL
+
+    # Install the package
+    echo "Installing $PACKAGE_NAME..."
+    sudo dpkg -i $PACKAGE_NAME
+
+    # Fix any broken dependencies
+    echo "Fixing dependencies..."
+    sudo apt-get install -f -y
+else
+    echo "[+++] wkhtmltopdf installed successfully."
+
+    # Define the URL for the wkhtmltox package (same as in the failure block)
     PACKAGE_URL="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb"
     PACKAGE_NAME="wkhtmltox_0.12.6-1.focal_amd64.deb"
 
@@ -34,23 +51,6 @@ if [ $? -ne 0 ]; then
 fi
 
 # Verify installation
-if command -v wkhtmltopdf &> /dev/null; then
-    echo "[+++]"
-    echo "[+++]wkhtmltopdf installed successfully."
-    echo "[+++]"
-else
-    echo "[---]"
-    echo "Failed to install wkhtmltopdf. Please check the errors above."
-    echo "[---]"
-fi
-
-#echo "[+++] Downloading Ravro Decrypt Tools..."
-#wget -q https://github.com/ravro-ir/ravro_dcrpt/releases/download/v1.0.3/linux_x64_ravro_dcrpt.zip
-
-#echo "[+++] Extracting Ravro Decrypt Tools..."
-#unzip -q linux_x64_ravro_dcrpt.zip -d ravro_dcrpt
-
-#echo "[+++] Cleanup..."
-#rm wkhtmltox_0.12.6.1-2.bullseye_amd64.deb linux_x64_ravro_dcrpt.zip
-
-echo "[+++] Installation complete!"
+echo "[+++]"
+echo "[+++] wkhtmltopdf installation process completed."
+echo "[+++]"
