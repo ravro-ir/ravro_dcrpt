90d4a23be4b39fef763f2cc6879e6308b6258bbb
raminfp <ramin.blackhat@gmail.com>
2024-12-21 13:38:35 +0330

    Update auto install linux

--- autoinstall/linux.sh
+++ autoinstall/linux.sh
@@ -3,23 +3,35 @@
 set -e
 
 echo "[+++] Updating package lists and installing dependencies..."
-sudo apt update && sudo apt install -y build-essential checkinstall zlib1g-dev openssl libssl-dev unzip wget
+sudo apt update -y
+sudo apt install -y build-essential checkinstall zlib1g-dev openssl libssl-dev unzip wget
 
-echo "[+++] Downloading wkhtmltox..."
-wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
+# Attempt to install wkhtmltopdf
+echo "Installing wkhtmltopdf..."
+sudo apt-get install -y wkhtmltopdf
 
-echo "[+++] Installing wkhtmltox..."
-sudo dpkg -i wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
-sudo apt-get install -f -y
-sudo ldconfig
+# Check for unmet dependencies and fix them if needed
+if [ $? -ne 0 ]; then
+    echo "Resolving unmet dependencies..."
+    sudo apt --fix-broken install -y
+    echo "Retrying installation of wkhtmltopdf..."
+    sudo apt-get install -y wkhtmltopdf
+fi
 
-echo "[+++] Downloading Ravro Decrypt Tools..."
-wget -q https://github.com/ravro-ir/ravro_dcrpt/releases/download/v1.0.3/linux_x64_ravro_dcrpt.zip
+# Verify installation
+if command -v wkhtmltopdf &> /dev/null; then
+    echo "wkhtmltopdf installed successfully."
+else
+    echo "Failed to install wkhtmltopdf. Please check the errors above."
+fi
 
-echo "[+++] Extracting Ravro Decrypt Tools..."
-unzip -q linux_x64_ravro_dcrpt.zip -d ravro_dcrpt
+#echo "[+++] Downloading Ravro Decrypt Tools..."
+#wget -q https://github.com/ravro-ir/ravro_dcrpt/releases/download/v1.0.3/linux_x64_ravro_dcrpt.zip
 
-echo "[+++] Cleanup..."
-rm wkhtmltox_0.12.6.1-2.bullseye_amd64.deb linux_x64_ravro_dcrpt.zip
+#echo "[+++] Extracting Ravro Decrypt Tools..."
+#unzip -q linux_x64_ravro_dcrpt.zip -d ravro_dcrpt
+
+#echo "[+++] Cleanup..."
+#rm wkhtmltox_0.12.6.1-2.bullseye_amd64.deb linux_x64_ravro_dcrpt.zip
 
 echo "[+++] Installation complete!"
